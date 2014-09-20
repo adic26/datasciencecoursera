99a9396fa07dcb33c0635317d3f7ac43c780e053
Aditya Chugh <chugh.aditya@gmail.com>
2014-09-20 10:44:03 -0400

    Graphing plot1 completed

--- Explatory Analysis/proj2/exdata-data-NEI_data/good.r
+++ Explatory Analysis/proj2/exdata-data-NEI_data/good.r
@@ -1,19 +1,26 @@
-myplot <- function()
+PLOT1 <- function()
 {
-  
-  plot( x = goodShit$year, y = goodShit$Emissions,
-        xlab = "",
+  #NEI <- readRDS("summarySCC_PM25.rds")
+  library(plyr)
+  allFlips <- ddply(NEI,.(type,year),numcolwise(sum))
+  plot( x = allFlips$year, y = allFlips$Emissions,
+        xlab = "Year",
         ylab = "Emissions",
-        type = "n")
-  
-  nonRoad <- goodShit[ goodShit$type == "NON-ROAD", ]
-  nonpoint <- goodShit[ goodShit$type == "NONPOINT", ]
-  onroad <- goodShit[ goodShit$type == "ON-ROAD", ]
-  point <- goodShit[ goodShit$type == "POINT", ]
+        type = "n"
+        )
+  title("Emissions for All Types")
+  nonRoad <- allFlips[ allFlips$type == "NON-ROAD", ]
+  nonpoint <- allFlips[ allFlips$type == "NONPOINT", ]
+  onroad <- allFlips[ allFlips$type == "ON-ROAD", ]
+  point <- allFlips[ allFlips$type == "POINT", ]
   lines( nonRoad$year, nonRoad$Emissions, col = "black" )
   lines( nonpoint$year, nonpoint$Emissions, col = "red" )
   lines( onroad$year, onroad$Emissions, col = "blue" )
   lines( point$year, point$Emissions, col = "green" )
+  legend( x="topright", 
+          legend=c("NON-ROAD","NONPOINT","ON-ROAD","POINT"),
+          col=c("black","red","blue", "green"), lwd=1, 
+          pch=c(NA,NA) )
   
 
 
